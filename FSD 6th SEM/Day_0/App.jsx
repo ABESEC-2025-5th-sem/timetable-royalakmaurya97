1c46e38512ac481d1f187a8bc894318b6e804638
Alok Kumar <mauryaak13@gmail.com>
2026-01-29 12:11:59 +0530

    Create index and app file

---
+++ FSD 6th SEM/Day_0/App.jsx
@@ -0,0 +1,9 @@
+console.log("Hii")
+const parent = document.getElementById('container');
+const root = ReactDOM.createRoot(parent)
+const h2 = React.createElement("h2", {}, "Welcome to React dev");
+const li1 = React.createElement("li1", {}, "Java Programming");
+const li2 = React.createElement("li2", {}, "C++ Programming");
+
+// Const img=React.createElement("img", {src:"./img1.jpg", style:})
+const ul = React.createElement("ul", )
