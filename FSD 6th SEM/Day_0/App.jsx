35f5f632b45eb3e8b26f27a048fc84ff78957038
Alok Kumar <mauryaak13@gmail.com>
2026-01-30 11:23:09 +0530

    Update  app file

--- FSD 6th SEM/Day_0/App.jsx
+++ FSD 6th SEM/Day_0/App.jsx
@@ -1,9 +1,15 @@
-console.log("Hii")
-const parent = document.getElementById('container');
-const root = ReactDOM.createRoot(parent)
-const h2 = React.createElement("h2", {}, "Welcome to React dev");
-const li1 = React.createElement("li1", {}, "Java Programming");
-const li2 = React.createElement("li2", {}, "C++ Programming");
+console.log("Hello");
 
-// Const img=React.createElement("img", {src:"./img1.jpg", style:})
-const ul = React.createElement("ul", )
+const parent = document.getElementById("container");
+const root = ReactDOM.createRoot(parent);
+
+setTimeout(() => {
+    const h2 = React.createElement("h2", {}, "Welcome to React dev");
+    const li1 = React.createElement("li", {}, "Java Programming");
+    const li2 = React.createElement("li", {}, "C++ Programming");
+
+    const ul = React.createElement("ul", {}, li1, li2);
+    const div = React.createElement("div", {}, h2, ul);
+
+    root.render(div);
+}, 2000);
