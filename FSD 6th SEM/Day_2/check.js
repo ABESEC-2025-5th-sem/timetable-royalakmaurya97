cb6fa99113ae288730d283b559b5782d021ed3bb
Alok Kumar <mauryaak13@gmail.com>
2026-02-05 11:23:18 +0530

    Function Call

---
+++ FSD 6th SEM/Day_2/check.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    console.log("Starting a task......");
+
+    let sum = 0;
+    for( let  i = 0; i < 1000000000; i++) {
+        sum += i;
+    }
+
+    console.log("Task Finished!");
+    return sum;
+}
+
+console.log("Before the function call");
+let result = myFunction();
+console.log("After the function call");
+console.log("Result: ", result);
